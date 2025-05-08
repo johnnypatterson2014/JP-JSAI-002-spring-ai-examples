ebf0fc55804bd46b3d43aad3261ed78b95ac597b
johnnypatterson2014 <johnny.patterson@gmail.com>
2025-05-07 23:14:41 -0700

    added RAG functionality

---
+++ src/main/java/com/calamansi/demo/controller/RagController.java
@@ -0,0 +1,27 @@
+package com.calamansi.demo.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rag")
+public class RagController {
+
+	private final ChatClient chatClient;
+
+	public RagController(ChatClient.Builder builder, VectorStore vectorStore) {
+		this.chatClient = builder.defaultAdvisors(new QuestionAnswerAdvisor(vectorStore)).build();
+	}
+
+	@GetMapping("/models")
+	public String ragQuery(
+			@RequestParam(value = "message", defaultValue = "List the top 3 Large Language Models when it comes to context window size.") String message) {
+		return chatClient.prompt().user(message).call().content();
+	}
+
+}
